8696b5425654bf62901b96a3764cf3c6fafd4752
Saifur Rahman <saifur80@gmail.com>
2018-07-11 12:22:18 +0600

    Updates

--- Performance/ASTRAL_I/AvgRunTime.R
+++ Performance/ASTRAL_I/AvgRunTime.R
@@ -1,26 +1,45 @@
-#########################################################
-# For writing to R window comment out the following line
-# in each graph
-# theme_bw(base_size = 36, base_family = "") +
-#########################################################
 
-fileName = c(
+rtQFMFiles = c(
   "rtQFM_scale5d.200g.500b.txt",
   "rtQFM_scale2d.200g.500b.txt",
   "rtQFM_noscale.200g.500b.txt",
   "rtQFM_scale2u.200g.500b.txt",
   "rtQFM_noscale.50g.500b.txt",
   "rtQFM_noscale.100g.500b.txt",
-  # "rtQFM_noscale.400g.500b.txt",
+  "rtQFM_noscale.400g.500b.txt",
   "rtQFM_noscale.800g.500b.txt",
   "rtQFM_noscale.200g.250b.txt",
   "rtQFM_noscale.200g.1000b.txt",
   "rtQFM_noscale.200g.true.txt"
   );
 
-for (file in fileName) {
+rtDcmQfmFiles = c(
+  "rtDcmQfm_scale5d.200g.500b.txt",
+  "rtDcmQfm_scale2d.200g.500b.txt",
+  "rtDcmQfm_noscale.200g.500b.txt",
+  "rtDcmQfm_scale2u.200g.500b.txt",
+  "rtDcmQfm_noscale.50g.500b.txt",
+  "rtDcmQfm_noscale.100g.500b.txt",
+  "rtDcmQfm_noscale.400g.500b.txt",
+  "rtDcmQfm_noscale.800g.500b.txt",
+  "rtDcmQfm_noscale.200g.250b.txt",
+  "rtDcmQfm_noscale.200g.1000b.txt",
+  "rtDcmQfm_noscale.200g.true.txt"
+);
+
+
+for (i in 1:length(rtQFMFiles)) {
+  qfmFile = rtQFMFiles[i];
+  dcmQfmFile = rtDcmQfmFiles[i];
+
+
+  data = read.csv(qfmFile);
+  qfmAvg = round(mean(data[,1]/60),2);
+
+  data = read.csv(dcmQfmFile);
+  dcmQfmSAvg = round(mean(data[,1]/60),2);
+  dcmQfmPAvg = round(mean(data[,2]/60),2);
   
-  data = read.csv(file);
-  avg = round(mean(data[,1]/60),2);
-  cat("", avg, "\n")
+  
+  cat(qfmFile, "        & ", qfmAvg, "  & ", dcmQfmSAvg, "         & ", dcmQfmPAvg, " \\\\ \\hline %", round(qfmAvg/dcmQfmSAvg, 1), round(qfmAvg/dcmQfmPAvg, 1), "\n");
 }
